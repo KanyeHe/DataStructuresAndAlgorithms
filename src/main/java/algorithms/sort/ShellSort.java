a617ad7d4371ba9e2ffd7dc376c79ff4c7a8ed85
Kanye <kanyehe@163.com>
2023-11-28 22:09:53 +0800

    希尔算法

--- src/main/java/algorithms/sort/ShellSort.java
+++ src/main/java/algorithms/sort/ShellSort.java
@@ -9,12 +9,14 @@ public class ShellSort {
 
     public static void main(String[] args) {
         int[] shell = {8, 9, 1, 7 ,2, 3, 5, 4, 6, 0};
-        shellSortByChange(shell);
+        //shellSortByChange(shell);
+        shellSortByMove(shell);
         System.out.println(Arrays.toString(shell));
     }
 
     /**
      * 希尔排序 交换法， 性能差
+     * 当 gap = 1 时，相当于做了一次冒泡排序
      * @param shell
      */
     private static void shellSortByChange(int[] shell) {
@@ -27,6 +29,27 @@ public class ShellSort {
                         shell[j] = shell[j + gap];
                         shell[j + gap] = temp;
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * 希尔排序 移动法， 性能较好
+     * @param shell
+     */
+    private static void shellSortByMove(int[] shell) {
+
+        for (int gap = shell.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < shell.length; i++) {
+                int j = i;
+                int temp = shell[j];
+                if (shell[j] < shell[j - gap]) {
+                    while (j - gap >= 0 && temp < shell[j - gap]) {
+                        shell[j] = shell[j - gap];
+                        j -= gap;
+                    }
+                    shell[j] = temp;
                 }
             }
         }
